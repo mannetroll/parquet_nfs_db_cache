7231c9e868774122d311a451c550aa73f494f9d1
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-06-06 15:38:54 +0200

    DataframeHolder

--- nfs_cache/data/dataframe_holder.py
+++ nfs_cache/data/dataframe_holder.py
@@ -1,10 +1,8 @@
 from typing import Any
+
 import polars as pl
 
-ROW = tuple[Any, ...]
-INPUT_DATA = dict[str, tuple | pl.DataFrame | ROW]
-
-class PolarsRowData:
+class DataframeHolder:
     headers: tuple
     rows_data_pl: pl.DataFrame | None = None
 
@@ -14,11 +12,11 @@ class PolarsRowData:
 
 
 if __name__ == "__main__":
-    INPUT_DATA = {
-        "headers": ["COL1", "COL2", "COL3"],
+    INPUT_DATA: dict[str, Any] = {
+        "headers": ("COL1", "COL2", "COL3"),
         "data": [(1.1, 1, "A"), (2.2, 2, "B"), (3.3, 3, "C"), (4.4, 4, "D")],
     }
-    data = PolarsRowData()
+    data = DataframeHolder()
     data.headers = tuple(INPUT_DATA["headers"])
     data.rows_data_pl = pl.DataFrame(INPUT_DATA["data"], schema=INPUT_DATA["headers"], orient="row")
     print("headers:", data.headers)
