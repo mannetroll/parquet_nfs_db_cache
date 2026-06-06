0f887e07e82320071d07c2364b88e3c4ad38160e
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-06-06 16:02:34 +0200

    DataContainer

--- nfs_cache/data/data_container.py
+++ nfs_cache/data/data_container.py
@@ -1,26 +1,26 @@
 from typing import Any
+
 import polars as pl
 
 from nfs_cache.data.data_holder import DataHolder
+
 
 class DataContainer:
     __slots__ = ("data",)
 
     def __init__(
-        self,
-        input_data: dict[str, tuple | tuple[Any, ...] | pl.DataFrame],
+            self,
+            input_data: dict[str, Any],
     ) -> None:
         self.data = DataHolder()
         self.data.headers = tuple(input_data["headers"])
-        self.data.meta_data = input_data.get("meta-data", None)
-
-        # store the actual table data, depending on orientation
         self.data.rows_data_pl = input_data["data"]
+
 
 if __name__ == "__main__":
     headers = ("COL1", "COL2", "COL3")
     rows = [(1.1, 1, "A"), (2.2, 2, "B"), (3.3, 3, "C"), (4.4, 4, "D")]
-    INPUT_DATA: dict[str, tuple[Any, ...] | pl.DataFrame] = {
+    INPUT_DATA: dict[str, Any] = {
         "headers": headers,
         "data": pl.DataFrame(rows, schema=headers, orient="row"),
     }
